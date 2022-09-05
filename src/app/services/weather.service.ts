620df682e51aeee0e786d00342b5adc393034b1a
Cyrine <boughanmycyrine@gmail.com>
2022-09-06 00:25:08 +0100

    adding RapidApi and setting the environement

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,5 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { WeatherData } from '../models/weather.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +10,15 @@ import { Injectable } from '@angular/core';
 export class WeatherService {
 
   constructor(private http : HttpClient) { }
+  getWeatherData (cityName : string) : Observable<WeatherData> {
+   return this.http.get<WeatherData>(environment.weatherApiBaseUrl, {
+      headers: new HttpHeaders()
+      .set(environment.XRapidAPIHostHeaderName , environment.XRapidAPIHostHeaderValue)
+      .set(environment.XRapidAPIKeyHeaderName,environment.XRapidAPIKeyHeaderValue),
+      params: new HttpParams()
+      .set('q',cityName)
+      .set('unitGroup','metric')
+      .set('mode','json')
+    })
+  }
 }
